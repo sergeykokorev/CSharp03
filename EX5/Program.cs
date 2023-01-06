b60157c987cb280d01b0748f20e2791640b93c41
Sergey.Kokorev <sergeykokorev03@gmail.com>
2023-01-06 18:16:10 +0300

    ex5

---
+++ EX5/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("enter user ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "maria")
+{
+    Console.WriteLine("Maria");
+}
+else
+{
+    Console.WriteLine("This is not Maria");
+}
