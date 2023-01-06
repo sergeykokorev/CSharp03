bfe200366b1400e441083c9a7b5cee2d62fc19c2
Sergey.Kokorev <sergeykokorev03@gmail.com>
2023-01-06 18:39:43 +0300

    ex6

---
+++ EX6/Program.cs
@@ -0,0 +1,18 @@
+﻿int a = 10;
+int b = 6;
+int c = 2;
+int d = 11;
+int e = 3;
+
+int max = a;
+
+if (a>max) max = a;
+if (b>max) max = b;
+if (c>max) max = c;
+if (d>max) max = d;
+if (e>max) max = e;
+
+Console.Write(max);
+
+
+
