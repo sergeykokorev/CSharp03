00160960ff9870e87be477895f0bde9e16041d6a
Sergey.Kokorev <sergeykokorev03@gmail.com>
2023-01-09 15:37:23 +0300

    Lesson1

--- Lesson1/Program.cs
+++ Lesson1/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine("ВВедите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("ВВедите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 == number2) Console.WriteLine("Числав равны между собой.");
+      
+    else if (number1 > number2)
+        {
+            Console.WriteLine($"Число {number1} больше числа {number2}");
+        }
+        else
+        {
+            Console.WriteLine($"Число {number1} меньше числа {number2}");
+        }
