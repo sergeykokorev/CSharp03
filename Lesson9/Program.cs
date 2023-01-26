8c39fe20b474485410ddca5e2c7dd7ecb6501604
sergeykokorev <sergeykokorev03@gmail.com>
2023-01-26 16:26:52 +0300

    add Lesson9

--- Lesson9/Program.cs
+++ Lesson9/Program.cs
@@ -15,21 +15,49 @@
 // }
 
 
-// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
-// M = 1; N = 15 -> 120
-// M = 4; N = 8. -> 30
+// // Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// // M = 1; N = 15 -> 120
+// // M = 4; N = 8. -> 30
 
 
-Console.WriteLine("Введите минимальное число:");
-int minNumber = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите максимальное число:");
-int maxNumber = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите минимальное число:");
+// int maxNumber = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите максимальное число:");
+// int minNumber = Convert.ToInt32(Console.ReadLine());
+// int sum = 0;
 
-Console.WriteLine(Numbers());
 
-void Numbers(int maxNumber, int minNumber, int sum)
+// Sum (maxNumber, minNumber);
+
+// void Sum (int numberM, int numberN)
+// {
+
+//     if (maxNumber > minNumber) 
+//     {
+//         Console.WriteLine($"Сумма чисел равна: {sum}"); return;
+//     }
+//     sum = sum + (maxNumber++);
+//     Sum(maxNumber, minNumber);
+// }
+
+
+// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+
+Console.WriteLine("Введите первое неотрицательное число:");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе неотрицательное число:");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(Akerman(number1,number2));
+
+int Akerman(int number1, int number2)
 {
-    int sumNumbers = 0;
-    if (maxNumber == minNumber) return sumNumbers;
-    sumNumbers = sumNumbers + Numbers(maxNumber - 1);
+    if (number1 == 0) return (number2 + 1);
+        else if ((number1 > 0) && (number2 == 0)) return Akerman(number1 - 1, 1);
+        else if ((number1 > 0) && (number2 > 0)) return Akerman(number1 - 1, Akerman(number1, number2 - 1));
+        else return number2 + 1;
 }
+
